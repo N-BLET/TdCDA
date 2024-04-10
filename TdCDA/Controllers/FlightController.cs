3498cc572224764a905a12da0901feb7a191d1f2
Nicolas BLET <nicolas.blet@hotmail.com>
2024-04-10 14:58:14 +0200

    Ajout de l'entité vol

--- TdCDA/Controllers/FlightController.cs
+++ TdCDA/Controllers/FlightController.cs
@@ -1,83 +1,216 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using TdCDA.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TdCDA.Controllers
 {
     public class FlightController : Controller
     {
+        IFirebaseClient? client;
+        // Interface de manipulation de la BDD Firebase
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "ZkMCzQKJrloO6GDnEWlx5qcwJjHJaNZ8ZcNZ2yaO",
+            BasePath = "https://bddtdcda-default-rtdb.europe-west1.firebasedatabase.app"
+        };
+
         // GET: FlightController
         public ActionResult Index()
         {
-            return View();
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Flights");
+            dynamic? data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Flight>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    Flight flight = JsonConvert.DeserializeObject<Flight>(((JProperty)item).Value.ToString()); 
+                    FirebaseResponse DepartureCityResponse = client.Get("Cities/" + flight.IdDepartureCity);
+                    City? depCity = JsonConvert.DeserializeObject<City>(DepartureCityResponse.Body);
+
+                    FirebaseResponse ArrivalCityResponse = client.Get("Cities/" + flight.IdArrivalCity);
+                    City? arrCity = JsonConvert.DeserializeObject<City>(ArrivalCityResponse.Body);
+
+                    flight.IdArrivalCity = arrCity.Name;
+                    flight.IdDepartureCity = depCity.Name;
+
+                    list.Add(flight);
+                    
+                }
+            }
+
+            return View(list);
         }
 
-        // GET: FlightController/Details/5
-        public ActionResult Details(int id)
+/*------------------------------------------- DETAILS ----------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------*/
+
+
+        // GET: FlightController/Details/id
+        public ActionResult Details(string id)
         {
-            return View();
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Flights/" + id);
+            Flight? flight = JsonConvert.DeserializeObject<Flight>(response.Body);
+
+            FirebaseResponse DepartureCityResponse = client.Get("Cities/" + flight.IdDepartureCity);
+            City? depCity = JsonConvert.DeserializeObject<City>(DepartureCityResponse.Body);
+
+            FirebaseResponse ArrivalCityResponse = client.Get("Cities/" + flight.IdArrivalCity);
+            City? arrCity = JsonConvert.DeserializeObject<City>(ArrivalCityResponse.Body);
+
+            flight.IdArrivalCity = arrCity.Name;
+            flight.IdDepartureCity = depCity.Name;
+
+            return View(flight);
         }
 
+/*------------------------------------------------------- CREATE ------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------*/
+        
         // GET: FlightController/Create
         public ActionResult Create()
         {
+            ViewData["IdDepartureCity"] = new SelectList(GetListCities(), "IdCity", "Name");
+            ViewData["IdArrivalCity"] = new SelectList(GetListCities(), "IdCity", "Name");
             return View();
         }
 
         // POST: FlightController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Flight flight)
         {
+            if(flight.IdDepartureCity == flight.IdArrivalCity)
+            {
+                ModelState.AddModelError("IdArrivalCity", "La ville de départ et d'arrivée ne peuvent pas être identiques.");
+                ViewData["IdDepartureCity"] = new SelectList(GetListCities(), "IdCity", "Name", flight.IdDepartureCity);
+                ViewData["IdArrivalCity"] = new SelectList(GetListCities(), "IdCity", "Name", flight.IdArrivalCity);
+                return View(flight);
+            }
             try
             {
+                client = new FireSharp.FirebaseClient(config);
+                var data = flight;
+                PushResponse response = client.Push("Flights/", flight);
+                flight.IdFlight = response.Result.name;
+                SetResponse setResponse = client.Set("Flights/" + flight.IdFlight, flight);
+
+                if (setResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    ModelState.AddModelError(string.Empty, "OK");
+                else
+                    ModelState.AddModelError(string.Empty, "KO!!");
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError(string.Empty, ex.Message);
             }
+
+            return RedirectToAction("Index");
         }
 
-        // GET: FlightController/Edit/5
-        public ActionResult Edit(int id)
+/*------------------------------------------------------- EDIT --------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------*/
+
+        // GET: FlightController/Edit/id
+        public ActionResult Edit(string id)
         {
-            return View();
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Flights/" + id);
+            Flight flight = JsonConvert.DeserializeObject<Flight>(response.Body);
+
+            FirebaseResponse DepartureCityResponse = client.Get("Cities/" + flight.IdDepartureCity);
+            City? depCity = JsonConvert.DeserializeObject<City>(DepartureCityResponse.Body);
+
+            FirebaseResponse ArrivalCityResponse = client.Get("Cities/" + flight.IdArrivalCity);
+            City? arrCity = JsonConvert.DeserializeObject<City>(ArrivalCityResponse.Body);
+
+            flight.IdArrivalCity = arrCity.Name;
+            flight.IdDepartureCity = depCity.Name;
+
+            return View(flight);
         }
 
-        // POST: FlightController/Edit/5
+        // POST: FlightController/Edit/id
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Flight flight)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Set("Livres/" + flight.IdFlight, flight);
+            return RedirectToAction("Index");
         }
 
-        // GET: FlightController/Delete/5
-        public ActionResult Delete(int id)
+/*------------------------------------------------------- DELETE ------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------*/
+        // GET: LivresController/Delete/id - pour confirmer la suppression d'un vol en fonction de son Id
+        // Affichage de la vue suppression
+        // GET: FlightController/Delete/id
+        public ActionResult Delete(string id)
         {
-            return View();
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Flights/" + id);
+            Flight flight = JsonConvert.DeserializeObject<Flight>(response.Body);
+
+            FirebaseResponse DepartureCityResponse = client.Get("Cities/" + flight.IdDepartureCity);
+            City? depCity = JsonConvert.DeserializeObject<City>(DepartureCityResponse.Body);
+
+            FirebaseResponse ArrivalCityResponse = client.Get("Cities/" + flight.IdArrivalCity);
+            City? arrCity = JsonConvert.DeserializeObject<City>(ArrivalCityResponse.Body);
+
+            flight.IdArrivalCity = arrCity.Name;
+            flight.IdDepartureCity = depCity.Name;
+
+            return View(flight);
         }
 
         // POST: FlightController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(string id, Flight flight)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Delete("Flights/" + id);
+            return RedirectToAction("Index");
         }
+
+        // <summary>
+        // Récupérere la liste des villes
+        // </summary>
+        // <returns></returns>
+        private List<City> GetListCities()
+        {
+            var list = new List<City>();
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Cities");
+            dynamic? data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<City>(((JProperty)item).Value.ToString()));
+                }
+            }
+            return list;
+        }
+
+        // 
+        private City GetCityById(string cityId)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Cities/" + cityId);
+            return response.ResultAs<City>();
+        }
+
     }
 }
--- TdCDA/Models/Flight.cs
+++ TdCDA/Models/Flight.cs
@@ -4,10 +4,10 @@
     {
         public string? IdFlight { get; set; }
         public string? Reference { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public  string? ArrivalDate { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public  DateTime? ArrivalDate { get; set; }
 
-        public virtual ICollection<Flight>? Vols { get; set; }
+        public virtual ICollection<Flight>? Flights { get; set; }
 
         public string? IdDepartureCity{ get; set; }
         public string? IdArrivalCity { get; set; }
